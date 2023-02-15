0155af561e6d14c976a7c2d9cac6aafaf636812f
karDante <caallop@hotmail.com>
2023-02-15 12:08:35 -0600

    Vlavla

--- SICIT.API.ENTITIES/Perfiles.cs
+++ SICIT.API.ENTITIES/Perfiles.cs
@@ -8,6 +8,7 @@ namespace SICIT.API.ENTITIES
 {
     public class Perfiles
     {
+        //Cambio CLL
         public int ID_PERFIL { get; set; }
         public string DESCRIPCION_PERFIL { get; set; }
         public int VIG_FLAG { get; set; }
