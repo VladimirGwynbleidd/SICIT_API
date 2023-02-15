da3bd30c4704de8d503e327854e357d70bec4ec5
karDante <caallop@hotmail.com>
2023-02-15 12:27:32 -0600

    Se agrega API para Monitor

---
+++ SICIT.API.BI/MonitorBI.cs
@@ -0,0 +1,21 @@
+﻿using SICIT.API.DAL;
+using SICIT.API.ENTITIES;
+using System;
+using System.Collections.Generic;
+
+
+namespace SICIT.API.BI.Interface
+{
+    public class MonitorBI
+    {
+        public Success<MonitorEntidad> Get()
+        {
+            Func<
+                FuncionDelegado<MonitorEntidad>.ObtenerResultado,
+                string,
+                IDictionary<string, object>,
+                Success<MonitorEntidad>> response = FuncionDelegado<MonitorEntidad>.obtenerListaResultado;
+            return response(new SqlHelperFactory().ExecuteList<MonitorEntidad>, ObjetosSQL.sp_obtenerMonitor, null);
+        }
+    }
+}
